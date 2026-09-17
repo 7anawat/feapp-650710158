8f85f468b918959e2dfd816fae780776d964598b
Tanawat Petpom <115211564+7anawat@users.noreply.github.com>
2026-09-17 05:16:58 +0000

    week7

--- thanawat-app/src/App.js
+++ thanawat-app/src/App.js
@@ -174,17 +174,22 @@ import MovieDetail from './pages/MovieDetail';
 import NotFound from './pages/NotFound';
 
 
+
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-slate-50">
       <Navbar />                {/* ← เห็นทุกหน้า */}
       <main className="flex-1">
         <Routes>
+          
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:id" element={<MovieDetail />} />
           <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />   {/* ← ดักทุกอย่างที่เหลือ ไว้ล่างสุด */}
+          <Route path="*" element={<NotFound />} />
+        
+             {/* ← ดักทุกอย่างที่เหลือ ไว้ล่างสุด */}
+          
         </Routes>
       </main>
       <Footer />                {/* ← เห็นทุกหน้า */}
